84b31350e57a6717e12fa6c80f03e1e097dbb752
vizsoro <vizsoro@gmail.com>
2016-09-14 09:18:31 +0200

    Small refactor in previewtestmethods.

--- src/test/java/vzsolt/statistics/selenium/PreviewTestMethods.java
+++ src/test/java/vzsolt/statistics/selenium/PreviewTestMethods.java
@@ -2,6 +2,8 @@ package vzsolt.statistics.selenium;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -12,10 +14,8 @@ public class PreviewTestMethods {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public static void testPreviewLength(WebDriver driver) {		
-		driver.findElement(By.id("file")).clear();
-		driver.findElement(By.id("file")).sendKeys("C:\\selenium\\sample-data.csv");
-		driver.findElement(By.id("upload")).click();
+	public static void testPreviewLength(WebDriver driver) {
+		openSampleData(driver);
 		JSChecker.waitForJStoLoad(driver);
 		long previewLengtsh = (long) ((JavascriptExecutor) driver).executeScript("return document.getElementsByTagName('tr').length");
 		
@@ -24,12 +24,17 @@ public class PreviewTestMethods {
 	
 	
 	public static void testPreviewWidth(WebDriver driver) {
-		driver.findElement(By.id("file")).clear();
-		driver.findElement(By.id("file")).sendKeys("C:\\selenium\\sample-data.csv");
-		driver.findElement(By.id("upload")).click();
+		openSampleData(driver);
 		JSChecker.waitForJStoLoad(driver);
 		long previewLengtsh = (long) ((JavascriptExecutor) driver).executeScript("return document.getElementsByTagName('td').length");
 		assertEquals(36, previewLengtsh);
+	}
+	
+	private static void openSampleData(WebDriver driver) {
+		driver.findElement(By.id("file")).clear();
+		String fullPath = new File(TestData.SAMPLE_DATA).getAbsolutePath();
+		driver.findElement(By.id("file")).sendKeys(fullPath);
+		driver.findElement(By.id("upload")).click();
 	}
 	
 }
